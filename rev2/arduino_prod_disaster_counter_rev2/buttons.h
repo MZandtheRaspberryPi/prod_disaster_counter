f99d7125f812f20d7744f1d0ace9d015756ac99f
MZandtheRaspberryPi <ziegltrum@gmail.com>
2022-07-26 00:43:02 +0200

    tuning button times

--- rev2/arduino_prod_disaster_counter_rev2/buttons.h
+++ rev2/arduino_prod_disaster_counter_rev2/buttons.h
@@ -18,7 +18,7 @@ namespace buttons
   struct button_settings_t
   {
     int button_press_threshold_1 = 1000; // under this threshold, we will increment day counter
-    int button_press_threshold_2 = 5000; // between threshold 1 and this, we will turn on night mode for leds. over this, we will clear counter.
+    int button_press_threshold_2 = 10000; // between threshold 1 and this, we will turn on night mode for leds. over this, we will clear counter.
   };
 
   static button_working_mem_t button_working_mem{};
